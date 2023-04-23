c86b0610550cd9d9a83137ec851d778076d2e547
yladev <yanlinnaung881@gmail.com>
2023-04-23 21:47:13 +0630

    chore: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ED7F39"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
